bc63cd08b59fd7de6b4a98203892778995b095e2
Maciej Janiszewski <pisarzk@gmail.com>
2013-08-22 22:38:31 +0200

    Introduce splashscreen

--- main.cpp
+++ main.cpp
@@ -17,11 +17,17 @@
 //#include "nativechaticon.h"
 #include "fileio.h"
 
+#include <QtGui/QSplashScreen>
+#include <QtGui/QPixmap>
+
 #define LIGHTBULB_NAMESPACE "lightbulb"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app( createApplication(argc, argv) );
+
+    QSplashScreen *splash = new QSplashScreen(QPixmap("qrc:/qml/images/splash.png"));
+    splash->show();
 
     qmlRegisterType<MyXmppClient>(LIGHTBULB_NAMESPACE, 1, 0, "XmppClient" );
     qmlRegisterType<MeegIMSettings>(LIGHTBULB_NAMESPACE, 1, 0, "MeegIMSettings" );
@@ -39,9 +45,17 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     qmlRegisterUncreatableType<AccountsListModel>(LIGHTBULB_NAMESPACE, 1, 0, "AccountsList", "Use settings.accounts instead");
 
     QmlApplicationViewer viewer;
+    viewer.setAttribute(Qt::WA_OpaquePaintEvent);
+    viewer.setAttribute(Qt::WA_NoSystemBackground);
+    viewer.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    viewer.viewport()->setAttribute(Qt::WA_NoSystemBackground);
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+
     viewer.setSource( QUrl(QLatin1String("qrc:/qml/main.qml")) );
-    viewer.showExpanded();
+    viewer.showFullScreen();
+
+    splash->finish(&viewer); //instead of &viewer write & and the name of your  QmlApplicationViewer or QDeclarativeView
+    splash->deleteLater();
 
     return app->exec();
 }
