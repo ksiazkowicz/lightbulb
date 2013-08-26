56ce0ff3ccc6ae828396c65b3b210e3aab85f58b
Maciej Janiszewski <pisarzk@gmail.com>
2013-08-26 23:07:50 +0200

    Use discreet popup to go back to app

--- discreetpopup.cpp
+++ discreetpopup.cpp
@@ -10,5 +10,5 @@ void discreetpopup::showPopup(QString title, QString message) {
     TPtrC16 sTitle(reinterpret_cast<const TUint16*>(title.utf16()));
     TPtrC16 sMessage(reinterpret_cast<const TUint16*>(message.utf16()));
 
-    TRAP_IGNORE(CAknDiscreetPopup::ShowGlobalPopupL(sTitle, sMessage,KAknsIIDNone, KNullDesC));
+    TRAP_IGNORE(CAknDiscreetPopup::ShowGlobalPopupL(sTitle, sMessage,KAknsIIDNone, KNullDesC, 0, 0, 180, 0, NULL, {0xE22AC278}));
 }
